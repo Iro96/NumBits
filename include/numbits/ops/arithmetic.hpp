e1a942ea57fd5b72ff4fa8a9cbec6e77842612ce
Iro <155815508+Iro96@users.noreply.github.com>
2025-10-11 21:45:27 +0700

    Update arithmetic.hpp

--- include/numbits/ops/arithmetic.hpp
+++ include/numbits/ops/arithmetic.hpp
@@ -1,24 +1,42 @@
 #pragma once
 #include "../core/ndarray.hpp"
 
-namespace nb {
+namespace numbits {
 
-template<typename T>
-ndarray<T> add(const ndarray<T>& a, const ndarray<T>& b) {
-    if (a.size() != b.size()) throw std::runtime_error("Shape mismatch in add()");
-    ndarray<T> out(a.shape());
-    for (size_t i = 0; i < a.size(); ++i)
-        out(i) = a(i) + b(i);
-    return out;
+template <typename T>
+ndarray<T> add(const ndarray<T>& A, const ndarray<T>& B) {
+    ndarray<T> C({A.shape()[0], A.shape()[1]});
+    for (size_t i = 0; i < A.shape()[0]; ++i)
+        for (size_t j = 0; j < A.shape()[1]; ++j)
+            C(i, j) = A(i, j) + B(i, j);
+    return C;
 }
 
-template<typename T>
-ndarray<T> multiply(const ndarray<T>& a, const ndarray<T>& b) {
-    if (a.size() != b.size()) throw std::runtime_error("Shape mismatch in multiply()");
-    ndarray<T> out(a.shape());
-    for (size_t i = 0; i < a.size(); ++i)
-        out(i) = a(i) * b(i);
-    return out;
+template <typename T>
+ndarray<T> sub(const ndarray<T>& A, const ndarray<T>& B) {
+    ndarray<T> C({A.shape()[0], A.shape()[1]});
+    for (size_t i = 0; i < A.shape()[0]; ++i)
+        for (size_t j = 0; j < A.shape()[1]; ++j)
+            C(i, j) = A(i, j) - B(i, j);
+    return C;
 }
 
-} // namespace nb
+template <typename T>
+ndarray<T> mul(const ndarray<T>& A, const ndarray<T>& B) {
+    ndarray<T> C({A.shape()[0], A.shape()[1]});
+    for (size_t i = 0; i < A.shape()[0]; ++i)
+        for (size_t j = 0; j < A.shape()[1]; ++j)
+            C(i, j) = A(i, j) * B(i, j);
+    return C;
+}
+
+template <typename T>
+ndarray<T> div(const ndarray<T>& A, const ndarray<T>& B) {
+    ndarray<T> C({A.shape()[0], A.shape()[1]});
+    for (size_t i = 0; i < A.shape()[0]; ++i)
+        for (size_t j = 0; j < A.shape()[1]; ++j)
+            C(i, j) = A(i, j) / B(i, j);
+    return C;
+}
+
+} // namespace numbits
