357d56c5778f19060240d32862ea94eba4081ed5
Iro <155815508+Iro96@users.noreply.github.com>
2025-10-11 21:44:08 +0700

    Update ndarray.hpp

--- include/numbits/core/ndarray.hpp
+++ include/numbits/core/ndarray.hpp
@@ -1,75 +1,54 @@
 #pragma once
 #include <vector>
 #include <iostream>
+#include <stdexcept>
 #include <initializer_list>
 #include <numeric>
-#include <stdexcept>
-#include <sstream>
 
-namespace nb {
+namespace numbits {
 
-template<typename T>
+template <typename T>
 class ndarray {
-private:
-    std::vector<size_t> shape_;
-    std::vector<T> data_;
-
 public:
     ndarray() = default;
 
-    explicit ndarray(std::vector<size_t> shape, T fill_value = T()) 
-        : shape_(std::move(shape)) {
-        size_t total = std::accumulate(shape_.begin(), shape_.end(), 1ul, std::multiplies<>());
-        data_.assign(total, fill_value);
-    }
-
-    ndarray(std::initializer_list<T> list)
-        : shape_{list.size()}, data_(list) {}
+    ndarray(std::initializer_list<size_t> shape, T init = T())
+        : shape_(shape),
+          data_(std::accumulate(shape.begin(), shape.end(), 1ul, std::multiplies<size_t>()), init) {}
 
     const std::vector<size_t>& shape() const { return shape_; }
+
     size_t size() const { return data_.size(); }
 
-    T& operator()(size_t i) { return data_[i]; }
-    const T& operator()(size_t i) const { return data_[i]; }
+    T& operator()(size_t i, size_t j) {
+        if (i >= shape_[0] || j >= shape_[1])
+            throw std::out_of_range("ndarray index out of bounds");
+        return data_[i * shape_[1] + j];
+    }
+
+    const T& operator()(size_t i, size_t j) const {
+        if (i >= shape_[0] || j >= shape_[1])
+            throw std::out_of_range("ndarray index out of bounds");
+        return data_[i * shape_[1] + j];
+    }
 
     std::vector<T>& data() { return data_; }
     const std::vector<T>& data() const { return data_; }
 
-    ndarray<T> operator+(const ndarray<T>& other) const {
-        if (data_.size() != other.data_.size()) throw std::runtime_error("Shape mismatch");
-        ndarray<T> out(shape_);
-        for (size_t i = 0; i < data_.size(); ++i)
-            out.data_[i] = data_[i] + other.data_[i];
-        return out;
-    }
-
-    ndarray<T> operator*(const ndarray<T>& other) const {
-        if (data_.size() != other.data_.size()) throw std::runtime_error("Shape mismatch");
-        ndarray<T> out(shape_);
-        for (size_t i = 0; i < data_.size(); ++i)
-            out.data_[i] = data_[i] * other.data_[i];
-        return out;
-    }
-
-    std::string str() const {
-        std::ostringstream os;
-        os << "ndarray([";
-        for (size_t i = 0; i < data_.size(); ++i) {
-            os << data_[i];
-            if (i + 1 < data_.size()) os << ", ";
+    friend std::ostream& operator<<(std::ostream& os, const ndarray& arr) {
+        size_t rows = arr.shape_[0], cols = arr.shape_[1];
+        for (size_t i = 0; i < rows; ++i) {
+            os << "[ ";
+            for (size_t j = 0; j < cols; ++j)
+                os << arr(i, j) << " ";
+            os << "]\n";
         }
-        os << "])";
-        return os.str();
+        return os;
     }
 
-    void print() const { std::cout << str() << std::endl; }
+private:
+    std::vector<size_t> shape_;
+    std::vector<T> data_;
 };
-
-template<typename T>
-ndarray<T> arange(T start, T stop, T step = 1) {
-    std::vector<T> data;
-    for (T i = start; i < stop; i += step) data.push_back(i);
-    return ndarray<T>({data.size()}, data);
-}
 
 } // namespace nb
