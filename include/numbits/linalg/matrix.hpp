18661dd3d33e5951052b65e0b1409d188a9a53e1
Iro <155815508+Iro96@users.noreply.github.com>
2025-10-11 22:23:58 +0700

    Update matrix.hpp

--- include/numbits/linalg/matrix.hpp
+++ include/numbits/linalg/matrix.hpp
@@ -1,17 +1,36 @@
 #pragma once
 #include "../core/ndarray.hpp"
+#include <stdexcept>
 
 namespace numbits {
 
 template <typename T>
 ndarray<T> dot(const ndarray<T>& A, const ndarray<T>& B) {
-    size_t n = A.shape()[0], m = B.shape()[1], p = A.shape()[1];
-    ndarray<T> C({n, m}, 0);
+    const auto& sA = A.shape();
+    const auto& sB = B.shape();
+
+    // Validate ranks (only 2D arrays supported)
+    if (sA.size() != 2 || sB.size() != 2)
+        throw std::invalid_argument("dot: expected two 2D ndarrays");
+
+    const size_t n = sA[0];
+    const size_t p = sA[1];
+    const size_t q = sB[0];
+    const size_t m = sB[1];
+
+    // Validate inner dimension match
+    if (p != q)
+        throw std::invalid_argument("dot: inner dimensions must match");
+
+    // Type-safe zero initialization
+    ndarray<T> C({n, m}, T{});
+
     for (size_t i = 0; i < n; ++i)
         for (size_t j = 0; j < m; ++j)
             for (size_t k = 0; k < p; ++k)
                 C(i, j) += A(i, k) * B(k, j);
+
     return C;
 }
 
-} // namespace nb
+} // namespace numbits
