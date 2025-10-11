a50609eb2fe0eb4e2aa0506b3adefcb903dcf5a4
Iro <155815508+Iro96@users.noreply.github.com>
2025-10-11 20:55:52 +0700

    Create numbits.hpp

---
+++ include/numbits/numbits.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "core/ndarray.hpp"
+#include "ops/arithmetic.hpp"
+#include "ops/reduction.hpp"
+#include "ops/math.hpp"
+#include "linalg/matrix.hpp"
+#include "stats/statistics.hpp"
+#include "random/generator.hpp"
