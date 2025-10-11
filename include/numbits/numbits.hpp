5f5dfcab737d8199624798ef7b9127b6829870fb
Iro <155815508+Iro96@users.noreply.github.com>
2025-10-11 21:43:38 +0700

    Update numbits.hpp

--- include/numbits/numbits.hpp
+++ include/numbits/numbits.hpp
@@ -1,8 +1,7 @@
 #pragma once
+// NumBits Master Header
 #include "core/ndarray.hpp"
 #include "ops/arithmetic.hpp"
 #include "ops/reduction.hpp"
-#include "ops/math.hpp"
-#include "linalg/matrix.hpp"
 #include "stats/statistics.hpp"
 #include "random/generator.hpp"
