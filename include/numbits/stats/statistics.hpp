73c72c7c27228f4492a14f1f3525ec5e164762e7
Iro <155815508+Iro96@users.noreply.github.com>
2025-10-11 22:50:12 +0700

    Update statistics.hpp

--- include/numbits/stats/statistics.hpp
+++ include/numbits/stats/statistics.hpp
@@ -1,21 +1,39 @@
 #pragma once
 #include "../core/ndarray.hpp"
-#include "../core/utils.hpp"    // for mean
+#include "../ops/reduction.hpp"  // for mean()
 #include <cmath>
+#include <type_traits>
 
 namespace numbits {
 
+/**
+ * @brief Compute the variance of elements in an ndarray.
+ *
+ * @tparam T Numeric type (usually float or double)
+ * @param A Input ndarray
+ * @return Variance of all elements
+ */
 template <typename T>
 T variance(const ndarray<T>& A) {
+    static_assert(std::is_arithmetic_v<T>, "variance requires a numeric type");
+
     T m = mean(A);
     T var = 0;
-    for (auto& x : A.data()) var += (x - m) * (x - m);
+    for (const auto& x : A.data())
+        var += (x - m) * (x - m);
     return var / static_cast<T>(A.size());
 }
 
+/**
+ * @brief Compute the standard deviation of an ndarray.
+ *
+ * @tparam T Numeric type (usually float or double)
+ * @param A Input ndarray
+ * @return Standard deviation
+ */
 template <typename T>
 T stddev(const ndarray<T>& A) {
     return std::sqrt(variance(A));
 }
 
-} // namespace nb
+} // namespace numbits
