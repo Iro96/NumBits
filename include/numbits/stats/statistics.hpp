fdcb75ed5a1818326d68df4749e2d9ae2aed6513
Iro <155815508+Iro96@users.noreply.github.com>
2025-10-11 21:48:01 +0700

    Update statistics.hpp

--- include/numbits/stats/statistics.hpp
+++ include/numbits/stats/statistics.hpp
@@ -1,15 +1,21 @@
 #pragma once
+#include "../core/ndarray.hpp"
 #include "../ops/reduction.hpp"
 #include <cmath>
 
-namespace nb {
+namespace numbits {
 
-template<typename T>
-double stddev(const ndarray<T>& a) {
-    double m = mean(a);
-    double accum = 0.0;
-    for (auto& v : a.data()) accum += (v - m) * (v - m);
-    return std::sqrt(accum / a.size());
+template <typename T>
+T variance(const ndarray<T>& A) {
+    T m = mean(A);
+    T var = 0;
+    for (auto& x : A.data()) var += (x - m) * (x - m);
+    return var / static_cast<T>(A.size());
+}
+
+template <typename T>
+T stddev(const ndarray<T>& A) {
+    return std::sqrt(variance(A));
 }
 
 } // namespace nb
