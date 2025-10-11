3b804c5e0ad9f4ad3620063a80dfd049a3a0ddf2
Iro <155815508+Iro96@users.noreply.github.com>
2025-10-11 21:48:35 +0700

    Update generator.hpp

--- include/numbits/random/generator.hpp
+++ include/numbits/random/generator.hpp
@@ -2,23 +2,16 @@
 #include "../core/ndarray.hpp"
 #include <random>
 
-namespace nb {
+namespace numbits {
 
-template<typename T = double>
-ndarray<T> rand(std::vector<size_t> shape) {
-    size_t total = 1;
-    for (auto s : shape) total *= s;
-    std::vector<T> data(total);
-
+template <typename T = double>
+ndarray<T> rand(const std::initializer_list<size_t>& shape) {
+    ndarray<T> A(shape);
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_real_distribution<T> dist(0.0, 1.0);
-
-    for (auto& v : data) v = dist(gen);
-    ndarray<T> result;
-    result = ndarray<T>(shape);
-    for (size_t i = 0; i < total; ++i) result.data()[i] = data[i];
-    return result;
+    for (auto& v : A.data()) v = dist(gen);
+    return A;
 }
 
 } // namespace nb
