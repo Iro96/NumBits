0ac8c35184173e08dbde404f9df0584105b5795d
Iro <155815508+Iro96@users.noreply.github.com>
2025-10-11 21:47:37 +0700

    Update matrix.hpp

--- include/numbits/linalg/matrix.hpp
+++ include/numbits/linalg/matrix.hpp
@@ -1,15 +1,17 @@
 #pragma once
 #include "../core/ndarray.hpp"
 
-namespace nb {
+namespace numbits {
 
-template<typename T>
-T dot(const ndarray<T>& a, const ndarray<T>& b) {
-    if (a.size() != b.size()) throw std::runtime_error("Shape mismatch in dot()");
-    T result = 0;
-    for (size_t i = 0; i < a.size(); ++i)
-        result += a(i) * b(i);
-    return result;
+template <typename T>
+ndarray<T> dot(const ndarray<T>& A, const ndarray<T>& B) {
+    size_t n = A.shape()[0], m = B.shape()[1], p = A.shape()[1];
+    ndarray<T> C({n, m}, 0);
+    for (size_t i = 0; i < n; ++i)
+        for (size_t j = 0; j < m; ++j)
+            for (size_t k = 0; k < p; ++k)
+                C(i, j) += A(i, k) * B(k, j);
+    return C;
 }
 
 } // namespace nb
